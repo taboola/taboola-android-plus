16ff1ff3e04143bb2c3af915aac68e145bc6bbb5
Tzahie <tzahie.l@taboola.com>
2020-04-22 21:00:15 +0300

    adding push notifications example
    Signed-off-by: Tzahie <tzahie.l@taboola.com>

---
+++ Examples/Push Notificaions Example/app/src/main/java/com/taboola/sample/notification/push/FBMessagingService.java
@@ -0,0 +1,48 @@
+package com.taboola.sample.notification.push;
+
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.taboola.android.plus.core.TBLPushManager;
+
+/**
+ * Base class for receiving messages from Firebase Cloud Messaging.
+ * Extending this class is required to be able to handle downstream messages
+ * . It also provides functionality to automatically display notifications,
+ * and has methods that are invoked to give the status of upstream messages.
+ * <p>
+ * Override base class methods to handle any events required by the application.
+ * All methods are invoked on a background thread,
+ * and may be called when the app is in the background or not open.
+ * <p>
+ * for more info please read
+ * https://firebase.google.com/docs/reference/android/com/google/firebase/messaging/FirebaseMessagingService
+ */
+public class FBMessagingService extends FirebaseMessagingService {
+    private static final String TAG = "FBMessagingService";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        //if the FCM RemoteMessage is not empty and sent by Taboola,
+        boolean isTaboolaMessage = TBLPushManager.isTaboolaMessage(remoteMessage);
+        if (isTaboolaMessage) {
+            // pass the remoteMessage to TBLPushManager so the sdk+ can handle it.
+            TBLPushManager.handleTaboolaPush(remoteMessage);
+        } else {
+            // this is not Taboola push, and you should handle it
+        }
+    }
+
+    @Override
+    public void onNewToken(@NonNull String token) {
+        Log.d(TAG, "onNewToken() called");
+        super.onNewToken(token);
+        TBLPushManager.onNewFirebaseToken(token);
+    }
+}
---
+++ Examples/Push Notificaions Example/app/src/main/java/com/taboola/sample/notification/push/SampleApplication.java
@@ -0,0 +1,71 @@
+package com.taboola.sample.notification.push;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+
+import com.google.firebase.FirebaseApp;
+import com.taboola.android.plus.core.PlusFeature;
+import com.taboola.android.plus.core.SdkPlusInitCallback;
+import com.taboola.android.plus.core.SdkPlusPublisherInfo;
+import com.taboola.android.plus.core.TaboolaSdkPlus;
+
+import static com.taboola.android.plus.core.PlusFeature.PUSH_NOTIFICATIONS;
+import static com.taboola.android.plus.core.PlusFeature.SCHEDULED_NOTIFICATIONS;
+import static com.taboola.sample.notification.push.UtilsHelper.ACTION_FOR_INIT_FINISH;
+import static com.taboola.sample.notification.push.UtilsHelper.EXTRA_FOR_INIT_FINISH;
+import static com.taboola.sample.notification.push.UtilsHelper.showToast;
+
+
+public class SampleApplication extends Application {
+
+    public static final String CONFIG_ID = "conf1";
+    public static final String PUB_NAME = "sdk-tester-plus";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        /* SDK Plus requires the app to use Firebase.
+         * Please make sure to integrate it according to the official FCM documentation
+         * https://firebase.google.com/docs/android/setup */
+        FirebaseApp.initializeApp(this);
+
+        //init sdk plus
+        initSdkPlus(this);
+    }
+
+
+    private void initSdkPlus(final Context appContext) {
+        SdkPlusPublisherInfo sdkPlusPublisherInfo = new SdkPlusPublisherInfo();
+        sdkPlusPublisherInfo.setConfigId(CONFIG_ID);
+        sdkPlusPublisherInfo.setPublisherName(PUB_NAME);
+        sdkPlusPublisherInfo.setActiveFeatures(PUSH_NOTIFICATIONS, SCHEDULED_NOTIFICATIONS);
+        SdkPlusInitCallback sdkPlusInitCallback = new SdkPlusInitCallback() {
+            @Override
+            public void onFeatureInitSuccessful(TaboolaSdkPlus taboolaSdkPlus, PlusFeature plusFeature) {
+                showToast(appContext, plusFeature.name() + " initialized");
+                notifySdkPlusInitFinished(plusFeature, appContext);
+            }
+
+            @Override
+            public void onFeatureInitFailed(PlusFeature plusFeature, Throwable throwable) {
+                final String text = plusFeature.name() + " init failed";
+                showToast(appContext, text);
+                // TODO: handle init failure in your code
+            }
+        };
+
+        TaboolaSdkPlus.init(sdkPlusPublisherInfo, sdkPlusInitCallback);
+    }
+
+
+    //notify SettingActivity that init is done
+    private void notifySdkPlusInitFinished(PlusFeature plusFeature, Context appContext) {
+            //notify sdk plus feature init is done, will be called separately for push and scheduled notifications
+            Intent intent = new Intent(ACTION_FOR_INIT_FINISH);
+            intent.putExtra(EXTRA_FOR_INIT_FINISH, plusFeature);
+            appContext.sendBroadcast(intent);
+    }
+
+}
