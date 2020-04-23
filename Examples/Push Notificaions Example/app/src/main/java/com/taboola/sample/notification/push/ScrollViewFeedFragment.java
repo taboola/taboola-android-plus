563d37744f4c55252dbacf4f7146a9bdd7774b87
Tzahie Leibovich <1096735+tzahie@users.noreply.github.com>
2020-04-23 21:19:00 +0300

    update sdk plus dependency to 2.2.1

---
+++ Examples/Push Notificaions Example/app/src/main/java/com/taboola/sample/notification/push/ScrollViewFeedFragment.java
@@ -0,0 +1,39 @@
+package com.taboola.sample.notification.push;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.taboola.android.TaboolaWidget;
+
+public class ScrollViewFeedFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_scrollview_feed, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TaboolaWidget widget = view.findViewById(R.id.taboolaWidget);
+        buildAndFetchFeedWidget(widget);
+    }
+
+    private void buildAndFetchFeedWidget(TaboolaWidget widget) {
+        widget.setPublisher("sdk-tester")
+                .setPageType("article")
+                .setPageUrl("https://blog.taboola.com")
+                .setPlacement("Feed without video")
+                .setMode("thumbs-feed-01")
+                .setTargetType("mix")
+                .fetchContent();
+    }
+}
